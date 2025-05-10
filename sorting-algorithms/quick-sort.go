cef91f0a0daf987f831abf8ad74de7dc680a18ad
Munene Kaumbutho <munene.kaumbutho@workjam.com>
2025-05-10 12:21:03 -0400

    main: discovered reason my quick sort algorithm was creating a final array that was too large

--- sorting-algorithms/quick-sort.go
+++ sorting-algorithms/quick-sort.go
@@ -2,6 +2,10 @@ package sorting
 
 import "fmt"
 
+// []int{5, 3, 8, 4, 2}
+// []int{5, 3, 4, 2, /8}
+// []int{5, 3, 4, 2, /8}
+
 func QuickSort(arr []int) []int {
 	fmt.Printf("init: %v \n\n", arr)
 
@@ -29,7 +33,6 @@ func QuickSort(arr []int) []int {
 
 		if vi <= pivot {
 			newArr[idxBeforePivot] = vi
-
 			idxBeforePivot++
 			newPivotIdx++
 		} else {
@@ -61,3 +64,6 @@ func QuickSort(arr []int) []int {
 
 // 6/5 - there is a problem in the algorithm in terms of how we place values before and
 //       after pivot. This requires further analysis.
+// 6/8 - it turns out that the append function on line 56 and 58 was increasing the size of the array, newArr
+//       This is why the lenght and capacity kept increasing. Not sure why the append was doing this and when
+//       append does this sort of thing vs. when it doesnt. Need to find out
